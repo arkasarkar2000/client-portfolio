0c603520655136b947060457b7c18c9141a0d8b2
arkasarkar2000 <sarkararka26@gmail.com>
2023-03-05 00:16:03 +0530

    first-client commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-texts",{
+    strings: ["Digital Marketer","Social Media Strategist"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true
+})
